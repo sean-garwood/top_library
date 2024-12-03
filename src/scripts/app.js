198e7ffbb98c4eafa901f027b485d539ab3d40dc
sean-garwood <seansgarwood@gmail.com>
2024-12-03 07:16:59 -0500

    feat: remove books from library
    Needs to be fixed: removes target and all succeeding books

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const newBookButton = document.querySelector('.new-book-button');
   const bookFormContainer = document.querySelector('.book-form-container');
 
+  // TODO: event listener to remove books from library
+  //   get a collection of all buttons with class remove-book-button
+  //   add an event listener to each button
+
+
   newBookButton.addEventListener('click', () => {
     fetch('book-form.html')
       .then(response => response.text())
@@ -18,6 +23,10 @@ document.addEventListener('DOMContentLoaded', () => {
           console.log(`${newBook} created`);
           addBookToLibrary(newBook);
           console.log(`${newBook} added to library`);
+          // FIXME: displays all books in library, so duplicates are shown
+          //   add a data attribute to each book-info-container to identify it
+          displayBook(newBook);
+          addEventListenersToRemoveBookButtons();
         });
       })
       .catch(error => console.error('Error loading the form:', error));
@@ -31,15 +40,6 @@ function Book(title, author, releaseYear, genre) {
   this.genre = genre;
 }
 
-function addBookToLibrary(bookForm) {
-  const book = makeBook(bookForm);
-  bookLibrary.push(book);
-}
-
-function getBook() {
-  bookLibrary.pop();
-}
-
 function makeBook() {
   // create book object from submitted book-info-form
   const title = document.getElementById("title").value;
@@ -51,6 +51,16 @@ function makeBook() {
   return book;
 }
 
+function addBookToLibrary(bookForm) {
+  const book = makeBook(bookForm);
+  bookLibrary.push(book);
+}
+
+function removeBookFromLibrary(book) {
+  const bookIndex = bookLibrary.indexOf(book);
+  bookLibrary[bookIndex] !== null && bookLibrary.splice(bookIndex, 1);
+}
+
 function displayBook(book) {
   const libContainer = document.getElementById("library-container");
   const bookInfoDiv = document.createElement("div");
@@ -58,15 +68,29 @@ function displayBook(book) {
   // generate html to display book attrs
   bookInfoDiv.innerHTML = `
     <h3>${book.title}</h3>
-    <p>${book.author}</p>
-    <p>${book.releaseYear}</p>
-    <p>${book.genre}</p>
+    <p>By ${book.author}</p>
+    <p>Released ${book.releaseYear}</p>
+    <p>Genre: ${book.genre}</p>
+    <button class="remove-book-button">Remove</button>
   `;
+  // add an attribute that uniquely identifies the book
+  bookInfoDiv.setAttribute('data-index', bookLibrary.indexOf(book) + bookLibrary.length);
   libContainer.appendChild(bookInfoDiv);
 }
 
 function displayBooks() {
   for (let step = 0; step < bookLibrary.length; step++) {
     displayBook(bookLibrary[step]);
+  }
+}
+
+function addEventListenersToRemoveBookButtons() {
+  let removeBookButtons = document.getElementsByClassName('remove-book-button');
+  for (let i = 0; i < removeBookButtons.length; i++) {
+    removeBookButtons[i].addEventListener('click', () => {
+      const bookIndex = removeBookButtons[i].parentNode.getAttribute('data-index');
+      removeBookFromLibrary(bookLibrary[bookIndex]);
+      removeBookButtons[i].parentNode.remove();
+    });
   }
 }
